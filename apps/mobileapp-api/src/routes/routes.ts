3af5becb5fa0123c19537ce967c40807dc28fd5b
joshuanatanielnm <clsjoshua@gmail.com>
2022-10-05 22:28:00 +0700

    updated: routes

--- apps/mobileapp-api/src/routes/routes.ts
+++ apps/mobileapp-api/src/routes/routes.ts
@@ -1,56 +1,57 @@
-import * as foodController from '../controllers/foodController';
 import * as userController from '../controllers/userController';
+import * as postingController from '../controllers/postingController';
+import * as commentController from '../controllers/commentController';
 
 import { RouteOptions } from 'fastify';
 
 type RouteConfig = Record<string, RouteOptions>;
 
 const routes: RouteConfig = {
-  getOfficialFood: {
-    method: 'GET',
-    url: '/officialfood',
-    handler: foodController.getOfficialFood,
-  },
-  getPersonalFood: {
-    method: 'GET',
-    url: '/personalfood',
-    handler: foodController.getPersonalFood,
-  },
-  addFood: {
-    method: 'POST',
-    url: '/addfood',
-    handler: foodController.addFood,
-  },
-  removeFood: {
-    method: 'DELETE',
-    url: '/removefood/:foodId',
-    handler: foodController.removeFood,
-  },
-  updateFood: {
-    method: 'PUT',
-    url: '/updateFood/:foodId',
-    handler: foodController.updateFood,
-  },
-  removeAllFood: {
-    method: 'DELETE',
-    url: '/removeallfood',
-    handler: foodController.removeAllFood,
-  },
-  getUserFood: {
-    method: 'GET',
-    url: '/food/:userId',
-    handler: foodController.getUserFood,
-  },
+  // getOfficialFood: {
+  //   method: 'GET',
+  //   url: '/officialfood',
+  //   handler: foodController.getOfficialFood,
+  // },
+  // getPersonalFood: {
+  //   method: 'GET',
+  //   url: '/personalfood',
+  //   handler: foodController.getPersonalFood,
+  // },
+  // addFood: {
+  //   method: 'POST',
+  //   url: '/addfood',
+  //   handler: foodController.addFood,
+  // },
+  // removeFood: {
+  //   method: 'DELETE',
+  //   url: '/removefood/:foodId',
+  //   handler: foodController.removeFood,
+  // },
+  // updateFood: {
+  //   method: 'PUT',
+  //   url: '/updateFood/:foodId',
+  //   handler: foodController.updateFood,
+  // },
+  // removeAllFood: {
+  //   method: 'DELETE',
+  //   url: '/removeallfood',
+  //   handler: foodController.removeAllFood,
+  // },
+  // getUserFood: {
+  //   method: 'GET',
+  //   url: '/food/:userId',
+  //   handler: foodController.getUserFood,
+  // },
   updateUser: {
     method: 'PUT',
     url: '/user/:userId',
     handler: userController.updateUser,
   },
-  getDetailFood: {
-    method: 'GET',
-    url: '/food/detailfood/:id',
-    handler: foodController.getDetailFood,
-  },
+  // getDetailFood: {
+  //   method: 'GET',
+  //   url: '/food/detailfood/:id',
+  //   handler: foodController.getDetailFood,
+  // },
   login: {
     method: 'POST',
     url: '/login',
@@ -70,6 +71,36 @@ const routes: RouteConfig = {
     method: 'DELETE',
     url: '/removealluser',
     handler: userController.removeAllUser,
+  },
+  getPosting: {
+    method: 'GET',
+    url: '/posting',
+    handler: postingController.getAllPosting,
+  },
+  getDetailPosting: {
+    method: 'GET',
+    url: '/posting/detailposting/:id',
+    handler: postingController.getDetailPosting,
+  },
+  getUserPosting: {
+    method: 'GET',
+    url: '/posting/:userId',
+    handler: postingController.getUserPosting,
+  },
+  addPosting: {
+    method: 'POST',
+    url: '/posting',
+    handler: postingController.addPosting,
+  },
+  removePosting: {
+    method: 'DELETE',
+    url: '/posting',
+    handler: postingController.removePosting,
+  },
+  addComment: {
+    method: 'POST',
+    url: '/comment',
+    handler: commentController.addComment,
   },
 };
 
